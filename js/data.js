9f4890c48c7c48d5b126ff25930f24b416867014
RIOTIX <luffysinbai@gmail.com>
2026-07-13 17:32:38 +0300

    Create data.js with character customization assets
    Added a data object containing arrays for clothes, hair, eyes, and shoes.

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const data = {
+    clothes: [
+        "clothes1.png",
+        "clothes2.png",
+        "clothes3.png"
+    ],
+
+    hair: [
+        { front: "front1.png", back: "back1.png" },
+        { front: "front2.png", back: "back2.png" }
+    ],
+
+    eyes: [
+        "eyes1.png",
+        "eyes2.png"
+    ],
+
+    shoes: [
+        "shoes1.png",
+        "shoes2.png"
+    ]
+};

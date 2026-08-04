1db1ada5c559e728be3650f156cb3f1b743b9a3d
RIOTIX <luffysinbai@gmail.com>
2026-08-04 14:35:45 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,30 +1,44 @@
 const data = {
 
-    clothes:[
+    clothes: [
         "dress1.png",
         "dress2.png",
-        "dress3.png"
+        "dress3.png",
+        "dress4.png",
+        "dress5.png",
+        "dress6.png",
+        "dress7.png",
+        "dress8.png"
     ],
 
-    shoes:[
-        "shoes1.png",
-        "shoes2.png"
+    hair: [
+        "front1.png",
+        "front2.png",
+        "front3.png",
+        "front4.png",
+        "front5.png",
+        "front6.png",
+        "front7.png"
     ],
 
-    eyes:[
+    eyes: [
         "eyes1.png",
-        "eyes2.png"
+        "eyes2.png",
+        "eyes3.png",
+        "eyes4.png",
+        "eyes5.png",
+        "eyes6.png",
+        "eyes7.png"
     ],
 
-    hair:[
-        {
-            front:"front1.png",
-            back:"back1.png"
-        },
-        {
-            front:"front2.png",
-            back:"back2.png"
-        }
+    shoes: [
+        "shoes1.png",
+        "shoes2.png",
+        "shoes3.png",
+        "shoes4.png",
+        "shoes5.png",
+        "shoes6.png",
+        "shoes7.png"
     ]
 
 };
